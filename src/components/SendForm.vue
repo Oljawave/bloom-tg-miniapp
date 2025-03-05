15964d52097e4449912996d9edb3293e5d68a000
Oljawave <misterolzhas04@gmail.com>
2025-03-05 14:33:44 +0600

    31 commit

--- src/components/SendForm.vue
+++ src/components/SendForm.vue
@@ -40,18 +40,18 @@
         </div>
         <div class="input-group">
           <label :class="{ active: apartment, label: true }">КВАРТИРА/ОФИС</label>
-          <input v-model="apartment" type="number" @input="validateNumber('apartment')" />
+          <input v-model="apartment" inputmode="numeric" pattern="[0-9]*" @input="validateNumber('apartment')" />
         </div>
       </div>
 
       <div class="row">
         <div class="input-group">
           <label :class="{ active: entrance, label: true }">ПОДЪЕЗД</label>
-          <input v-model="entrance" type="number" @input="validateNumber('entrance')" />
+          <input v-model="entrance" inputmode="numeric" pattern="[0-9]*" @input="validateNumber('entrance')" />
         </div>
         <div class="input-group">
           <label :class="{ active: floor, label: true }">ЭТАЖ</label>
-          <input v-model="floor" type="number" @input="validateNumber('floor')" />
+          <input v-model="floor" inputmode="numeric" pattern="[0-9]*" @input="validateNumber('floor')" />
         </div>
       </div>
   
